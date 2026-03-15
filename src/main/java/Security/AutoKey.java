441454e2e1f519cf6524ef261e5a9e75db6f088d
joeragheb29 <youssifragheb838@gmail.com>
2026-03-16 00:35:01 +0200

    autoket analysis and decrypt

--- src/main/java/Security/AutoKey.java
+++ src/main/java/Security/AutoKey.java
@@ -3,11 +3,30 @@ package Security;
 public class AutoKey {
     public String analyse(String plainText, String cipherText) {
         // Students should complete this part
-        return null;
+        String key = "";
+        for (int i = 0; i < plainText.length(); i++) {
+
+            char p = Character.toLowerCase(plainText.charAt(i));
+            char c = Character.toLowerCase(cipherText.charAt(i));
+
+            key += (char) ((c - p + 26) % 26 + 'a');
+        }
+
+        StringBuilder analysis = new StringBuilder();
+        for (int i = 0; i < plainText.length(); i++) {
+            char p = plainText.charAt(i);
+            char c = cipherText.charAt(i);
+            analysis.append(p).append(" -> ").append(c).append("\n");
+        }
+        return analysis.toString();
     }
 
     public String decrypt(String cipherText, String key) {
         // Students should complete this part
+        cipherText = cipherText.toLowerCase();
+        key = key.toLowerCase();
+
+        encrypt(key , cipherText);
         return null;
     }
 
