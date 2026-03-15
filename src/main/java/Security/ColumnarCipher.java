91a88a5d303e711be9b47dd58ec16eec057622b6
joeragheb29 <youssifragheb838@gmail.com>
2026-03-16 01:21:47 +0200

    columnar analysis

--- src/main/java/Security/ColumnarCipher.java
+++ src/main/java/Security/ColumnarCipher.java
@@ -4,9 +4,69 @@ import java.util.*;
 public class ColumnarCipher {
 
     public List<Integer> analyse(String plainText, String cipherText) {
-        // TODO: Analyze the plainText and cipherText to determine the key(s)
+        int cipherLen = cipherText.length();
+        
+        for (int keySize = 1; keySize <= cipherLen; keySize++) {
+            if (cipherLen % keySize != 0) continue;
 
-        return new ArrayList<>(); // Placeholder return
+            int rows = cipherLen / keySize;
+            
+            StringBuilder paddedPt = new StringBuilder(plainText);
+            while (paddedPt.length() < cipherLen) {
+                paddedPt.append('x');
+            }
+            
+            char[][] grid = new char[rows][keySize];
+            int count = 0;
+            for (int r = 0; r < rows; r++) {
+                for (int c = 0; c < keySize; c++) {
+                    grid[r][c] = paddedPt.charAt(count++);
+                }
+            }
+
+            List<String> ptColumns = new ArrayList<>();
+            for (int c = 0; c < keySize; c++) {
+                StringBuilder col = new StringBuilder();
+                for (int r = 0; r < rows; r++) {
+                    col.append(grid[r][c]);
+                }
+                ptColumns.add(col.toString().toUpperCase());
+            }
+
+            List<String> ctChunks = new ArrayList<>();
+            for (int i = 0; i < keySize; i++) {
+                ctChunks.add(cipherText.substring(i * rows, (i + 1) * rows).toUpperCase());
+            }
+
+            Integer[] keyArray = new Integer[keySize];
+            boolean[] used = new boolean[keySize];
+            boolean possible = true;
+
+            for (int i = 0; i < keySize; i++) {
+                String chunk = ctChunks.get(i);
+                int matchIdx = -1;
+                for (int j = 0; j < ptColumns.size(); j++) {
+                    if (!used[j] && ptColumns.get(j).equals(chunk)) {
+                        matchIdx = j;
+                        break;
+                    }
+                }
+
+                if (matchIdx != -1) {
+                    keyArray[matchIdx] = i + 1;
+                    used[matchIdx] = true;
+                } else {
+                    possible = false;
+                    break;
+                }
+            }
+
+            if (possible) {
+                return Arrays.asList(keyArray);
+            }
+        }
+
+        return new ArrayList<>();
     }
 
     public String decrypt(String cipherText, List<Integer> key) {
